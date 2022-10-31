e4a5a8be2cd6650c6a42231d98e26b3215eba4cc
peatchXD <100938925+peatchXD@users.noreply.github.com>
2022-10-31 14:18:14 +0700

    Update Roblox Script Build-A-Boat-For-Treasure.lua

--- Roblox Script Build-A-Boat-For-Treasure.lua
+++ Roblox Script Build-A-Boat-For-Treasure.lua
@@ -12,7 +12,29 @@ Section:NewButton("Click", "Script Auto Builder", function() loadstring(game:Htt
 local Section = Tab:NewSection("Script Auto Builder Image")
 
 Section:NewButton("Click", "Script Auto Builder Image", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure-Fake-ALL/main/Auto%20Builder%20Image.lua"))() end)
+--
+local Tab = Window:NewTab("🌌 Teleport To Teams")
+local Section = Tab:NewSection("White")
+Section:NewButton("Click", "NONAME HUB", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-50.0028458, -9.65999889, -567.075378, -0.999934494, 1.69590315e-08, -0.0114457831, 1.56394382e-08, 1, 1.15380317e-07, 0.0114457831, 1.15193757e-07, -0.999934494)
 
+local Section = Tab:NewSection("Black")
+Section:NewButton("Click", "NONAME HUB", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-550.587769, -9.65999889, -69.4330597, 0.044106476, -2.16794671e-09, -0.999026835, -4.38382752e-09, 1, -2.36360198e-09, 0.999026835, 4.48381154e-09, 0.044106476)
+
+local Section = Tab:NewSection("Red")
+Section:NewButton("Click", "NONAME HUB", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(443.415039, -9.65999889, -64.935936, -0.0039063096, -1.38479038e-07, 0.999992371, 2.78122503e-08, 1, 1.38588746e-07, -0.999992371, 2.83534085e-08, -0.0039063096)
+
+local Section = Tab:NewSection("Blue")
+Section:NewButton("Click", "NONAME HUB", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(443.401245, -9.65999889, 300.047424, 0.0413250774, -1.24215788e-07, 0.999145746, 3.61023424e-08, 1, 1.2282878e-07, -0.999145746, 3.09955936e-08, 0.0413250774)
+                
+local Section = Tab:NewSection("Green")
+Section:NewButton("Click", "NONAME HUB", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( -550.645752, -9.65999889, 293.29657, 0.0522636063, 3.91167561e-08, -0.998633325, -3.50806033e-08, 1, 3.73343418e-08, 0.998633325, 3.30814345e-08, 0.0522636063)
+
+local Section = Tab:NewSection("Yellow")
+Section:NewButton("Click", "NONAME HUB", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-550.60083, -9.65999889, 640.269409, 0.0527031086, 5.94260179e-08, -0.998610198, 9.28528578e-08, 1, 6.44091642e-08, 0.998610198, -9.61183773e-08, 0.0527031086)               
+
+local Section = Tab:NewSection("Magenta")
+Section:NewButton("Click", "NONAME HUB", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(443.725342, -9.89999676, 647.826416, -0.00752122747, -6.9397494e-08, 0.999971688, -4.07260288e-08, 1, 6.90931401e-08, -0.999971688, -4.02052116e-08, -0.00752122747)            
+--
 local Tab = Window:NewTab("👑 Fake") 
 local Section = Tab:NewSection("Fake Gold")
 
