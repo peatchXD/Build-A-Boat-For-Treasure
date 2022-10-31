80461f82f0bfd9306ecb8d5bb91219d45c30a2f1
peatchXD <100938925+peatchXD@users.noreply.github.com>
2022-10-31 23:23:43 +0700

    Update Build-A-Boat-For-Treasure Noname Hub.lua

--- Build-A-Boat-For-Treasure Noname Hub.lua
+++ Build-A-Boat-For-Treasure Noname Hub.lua
@@ -1,3 +1,18 @@
+local Server =  syn.request({
+    Url = "http://127.0.0.1/WHITELIST%20NONAME%20HUB/Server.php?Key=".. _G.Key .."&HWID="..HWID,
+    Method = "GET"
+}).Body
+
+if Server == "WHITELIST !" then
+    print("SCRIPT")
+elseif Server == "Invaid HWID !" then
+    game.Players.LocalPlayer:kick("Invaid HWID")
+elseif Server == "Invaid Key" then
+    game.Players.LocalPlayer:kick("Invaid Key")
+else
+    game.Players.LocalPlayer:kick("Invaid Key")
+end
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/GUI"))() 
 local Window = Library.CreateLib("NONAME HUB", "DarkTheme")
 
