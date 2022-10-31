2ddc11930e9f6d565828657708c91b995d11b2a7
peatchXD <100938925+peatchXD@users.noreply.github.com>
2022-10-31 21:13:50 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure-Fake-ALL/main/Roblox%20Script%20Build-A-Boat-For-Treasure.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/Roblox%20Script%20Build-A-Boat-For-Treasure.lua"))()
