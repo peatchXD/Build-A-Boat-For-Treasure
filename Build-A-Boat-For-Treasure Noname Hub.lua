03764544b90346c5805b98cc1b799be5ef431654
peatchXD <100938925+peatchXD@users.noreply.github.com>
2022-11-03 17:25:24 +0700

    Update Build-A-Boat-For-Treasure Noname Hub.lua

--- Build-A-Boat-For-Treasure Noname Hub.lua
+++ Build-A-Boat-For-Treasure Noname Hub.lua
@@ -3,6 +3,29 @@ local Window = Library.CreateLib("NONAME HUB", "DarkTheme")
 
 local Tab = Window:NewTab("💳 Auto Farm") local Section = Tab:NewSection("🧧 Script Auto Farm")
 Section:NewButton("Click", " Auto Farm Script", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/Auto%20Farm.lua"))() end)
+Section:NewButton("TEST FARM", " Auto Farm Script", function() local players = game:GetService("Players")
+local plr = players.LocalPlayer
+local tween = game:GetService("TweenService")
+game.Workspace.Gravity = 0.5 -- reduces glitchiness
+function main()
+	local humroot = plr.Character:WaitForChild("HumanoidRootPart")
+	local startTween = tween:Create(humroot, TweenInfo.new(4, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(-51.3946571, 67.3164978, 814.888123, -0.999501824, -0.00451373775, 0.0312365349, -8.62000427e-09, 0.989720345, 0.14301616, -0.0315609723, 0.142944917, -0.989227295)})
+	startTween:Play()
+	startTween.Completed:Wait()
+	local mainTween = tween:Create(humroot, TweenInfo.new(20.50, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(-77.0485153, 82.6013031, 8625.86719, -0.995574772, 0.022579968, -0.0912195817, -4.97565011e-09, 0.970703065, 0.240282282, 0.0939726979, 0.23921898, -0.966407478)})
+	mainTween:Play()
+	mainTween.Completed:Wait()
+	local endTween = tween:Create(humroot, TweenInfo.new(4, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = game:GetService("Workspace").BoatStages.NormalStages.TheEnd.GoldenChest.Trigger.CFrame})
+	endTween:Play()
+	endTween.Completed:Wait()
+end
+main()
+plr.CharacterAdded:Connect(function(char)
+	char:WaitForChild("HumanoidRootPart")
+	main()
+end)
+end)
+
 
 local Tab = Window:NewTab("🔨 Auto Builder") 
 local Section = Tab:NewSection("🧱 Script Auto Builder")
