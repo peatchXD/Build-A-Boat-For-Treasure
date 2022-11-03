ed3b54ce991c8d5fd4c7be48e694afdf97465242
peatchXD <100938925+peatchXD@users.noreply.github.com>
2022-11-03 17:31:14 +0700

    Update Build-A-Boat-For-Treasure Noname Hub.lua

--- Build-A-Boat-For-Treasure Noname Hub.lua
+++ Build-A-Boat-For-Treasure Noname Hub.lua
@@ -3,27 +3,76 @@ local Window = Library.CreateLib("NONAME HUB", "DarkTheme")
 
 local Tab = Window:NewTab("💳 Auto Farm") local Section = Tab:NewSection("🧧 Script Auto Farm")
 Section:NewButton("Click", " Auto Farm Script", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/peatchXD/Build-A-Boat-For-Treasure/main/Auto%20Farm.lua"))() end)
-Section:NewToggle("TEST FARM", " Auto Farm Script", function() local players = game:GetService("Players")
-local plr = players.LocalPlayer
-local tween = game:GetService("TweenService")
-game.Workspace.Gravity = 0.5 -- reduces glitchiness
-function main()
-	local humroot = plr.Character:WaitForChild("HumanoidRootPart")
-	local startTween = tween:Create(humroot, TweenInfo.new(4, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(-51.3946571, 67.3164978, 814.888123, -0.999501824, -0.00451373775, 0.0312365349, -8.62000427e-09, 0.989720345, 0.14301616, -0.0315609723, 0.142944917, -0.989227295)})
-	startTween:Play()
-	startTween.Completed:Wait()
-	local mainTween = tween:Create(humroot, TweenInfo.new(20.50, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(-77.0485153, 82.6013031, 8625.86719, -0.995574772, 0.022579968, -0.0912195817, -4.97565011e-09, 0.970703065, 0.240282282, 0.0939726979, 0.23921898, -0.966407478)})
-	mainTween:Play()
-	mainTween.Completed:Wait()
-	local endTween = tween:Create(humroot, TweenInfo.new(4, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = game:GetService("Workspace").BoatStages.NormalStages.TheEnd.GoldenChest.Trigger.CFrame})
-	endTween:Play()
-	endTween.Completed:Wait()
+Section:NewToggle("TEST Teleport FARM", " Auto Farm Script", function() -- // Custom Settings
+getgenv().TreasureAutoFarm = {
+    Enabled = true, -- // Toggle the auto farm on and off
+    Teleport = 2, -- // How fast between each teleport between the stages and stuff
+    TimeBetweenRuns = 5 -- // How long to wait until it goes to the next run
+}
+
+-- // Services
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+local Lighting = game:GetService("Lighting")
+
+-- // Vars
+local LocalPlayer = Players.LocalPlayer
+
+-- // Goes through all of the stages
+local autoFarm = function(currentRun)
+    -- // Variables
+    local Character = LocalPlayer.Character
+    local NormalStages = Workspace.BoatStages.NormalStages
+
+    -- // Go to each stage thing
+    for i = 1, 10 do
+        local Stage = NormalStages["CaveStage" .. i]
+        local DarknessPart = Stage:FindFirstChild("DarknessPart")
+
+        if (DarknessPart) then
+            -- // Teleport to next stage
+            print("Teleporting to next stage: Stage " .. i)
+            Character.HumanoidRootPart.CFrame = DarknessPart.CFrame
+
+            -- // Create a temp part under you
+            local Part = Instance.new("Part", LocalPlayer.Character)
+            Part.Anchored = true
+            Part.Position = LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0, 6, 0)
+
+            -- // Wait and remove temp part
+            wait(getgenv().TreasureAutoFarm.Teleport)
+            Part:Destroy()
+        end
+    end
+
+    -- // Go to end
+    print("Teleporting to the end")
+    repeat wait()
+        Character.HumanoidRootPart.CFrame = NormalStages.TheEnd.GoldenChest.Trigger.CFrame
+    until Lighting.ClockTime ~= 14
+
+    -- // Wait until you have respawned
+    local Respawned = false
+    local Connection
+    Connection = LocalPlayer.CharacterAdded:Connect(function()
+        Respawned = true
+        Connection:Disconnect()
+    end)
+
+    repeat wait() until Respawned
+    wait(getgenv().TreasureAutoFarm.TimeBetweenRuns)
+    print("Auto Farm: Run " .. currentRun .. " finished")
 end
-main()
-plr.CharacterAdded:Connect(function(char)
-	char:WaitForChild("HumanoidRootPart")
-	main()
-end)
+
+-- // Whilst the autofarm is enable, constantly do it
+local autoFarmRun = 1
+while wait() do
+    if (getgenv().TreasureAutoFarm.Enabled) then
+        print("Initialising Auto Farm: Run " .. autoFarmRun)
+        autoFarm(autoFarmRun)
+        autoFarmRun = autoFarmRun + 1
+    end
+end
 end)
 
 
